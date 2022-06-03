f433971f53787b982f2550278735fa7087a0bfcd
Nando1999 <fjmasache@espe.edu.ec>
2022-06-02 22:06:05 -0500

    constructores

--- grafoBFS.py
+++ grafoBFS.py
@@ -29,3 +29,30 @@ class Grafo:
     busqueda_por_Amplitud(nodo_Inicio):
         Primera búsqueda por amplitud o anchura
     """
+
+     # Constructor
+    def __init__(self, num_Nodos, dirigido=True):
+        """
+        Método constructor de la clase Grafo, donde inicializa sus parámetros
+
+        Parámetros
+            ----------
+            num_Nodos : int
+                Número de nodos del grafo
+            dirigido : boolean
+                Tipo de grafo dirigido o no dirigido
+        """
+        # Define el número de nodos
+        self.m_num_Nodos = num_Nodos
+        # Define el rango del número de nodos
+        self.m_Nodos = range(self.m_num_Nodos)
+
+        # Tipo de grafo dirigido o no dirigido
+        self.m_dirigido = dirigido
+
+        # Representación de la lista de adyacencia
+        # Para la implementación de la lista de adyacencia se usa un diccionario
+        self.m_lista_adyacencia = {node: set() for node in self.m_Nodos}
+
+    # Añande una arista al grafo
+    def agregar_Arista(self, nodo1, nodo2, peso=1):
